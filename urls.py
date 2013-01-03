97de174799cb719c662e16a6022b2682661bce30
Stephan Hoyer <ste.hoyer@gmail.com>
2013-01-03 17:28:27 +0100

    Add list template and include it

--- urls.py
+++ urls.py
@@ -4,5 +4,7 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.BaseView.as_view(), name='base'),
     url(r'^a/', views.AView.as_view(), name='a'),
+    url(r'^a/a', views.AAView.as_view(), name='aa'),
+    url(r'^a/b', views.ABView.as_view(), name='ab'),
     url(r'^b/', views.BView.as_view(), name='b'),
 )
